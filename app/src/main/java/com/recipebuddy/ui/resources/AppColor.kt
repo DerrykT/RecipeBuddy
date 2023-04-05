b4929d30a2db6b3273976c4dd48bc226132f822d
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2023-04-05 15:07:31 -0400

    Add files via upload

---
+++ app/src/main/java/com/recipebuddy/ui/resources/AppColor.kt
@@ -0,0 +1,12 @@
+package com.recipebuddy.ui.resources
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val BACKGROUND_PRIMARY = Color(240, 197, 151) // #facd9d
+    val BACKGROUND_SECONDARY = Color(214, 156, 94) // #d69c5e
+    val BUTTON_OUTLINE = Color(158, 105, 49) // #9e6931
+
+    val TAG_ICON_LIGHT_ORANGE = Color(247, 149, 12) // #f7950c
+    val TAG_BUTTON_GREEN = Color(48, 140, 14) // #308c0e
+}
