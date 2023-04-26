82b9c66893a512b182cd1d93ddbc3f6d249cb4cd
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2023-04-26 19:57:40 -0400

    Login with Database
    The database instance grab here might be redundant and could be taken out later if it's not needed.

--- app/src/main/java/com/recipebuddy/KotlinMainActivity.kt
+++ app/src/main/java/com/recipebuddy/KotlinMainActivity.kt
@@ -1,6 +1,8 @@
 package com.recipebuddy
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -8,12 +10,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.room.Room
+import com.recipebuddy.components.AppDatabase
 import com.recipebuddy.components.HomeScreen
 import com.recipebuddy.components.ProfileHomeScreen
 
+lateinit var db: AppDatabase
 class KotlinMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        db = AppDatabase.getInstance(this)
+
         setContent {
             HomeScreen()
         }
