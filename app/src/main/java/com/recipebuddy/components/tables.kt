9a08db5f6ce2d822b0ab288e32acedf70c31f25d
Henry <henrywrausch@gmail,com>
2023-04-26 17:59:02 -0400

    Tinkerable version

--- app/src/main/java/com/recipebuddy/components/tables.kt
+++ app/src/main/java/com/recipebuddy/components/tables.kt
@@ -4,54 +4,42 @@ import androidx.room.*
 
 @Entity(tableName = "Recipe_Info")
 data class Recipe_Info(
-    @PrimaryKey(autoGenerate = true) val RecipeID: Int,
-    val RecipeName: String,
+    @PrimaryKey val RecipeName: String,
     val RecipeDesc: String,
     val RecipeInstructions: String,
     val RecipeRating: Double,
-    val AssociatedUser: String
+    val AssociatedUser: String,
+    val Time: String,
+    val Picture: Int,
+    val Tools: String
 )
 
 @Entity(tableName = "Ingredient_List")
 data class Ingredient_List(
-    @PrimaryKey(autoGenerate = true) val IngredientID: Int,
-    val IngredientName: String
+    @PrimaryKey val IngredientName: String,
+    val Quantity: Int,
+    val Unit: String
 )
 
 @Entity(tableName = "Ingredient_Use")
 data class Ingredient_Use(
     @PrimaryKey(autoGenerate = true) val UsageKey: Int,
-    val IngredientID: Int, // Electing not to make a FK here, means incomplete data doesn't break instantly
-    val RecipeID: Int,
+    val IngredientName: String,
+    val RecipeName: String,
     val Quantity: Int,
     val Unit: String
 )
 
-@Entity(tableName = "Recipe_Ingredients")
-data class Recipe_Ingredients(
-    @PrimaryKey(autoGenerate = true) val PairKey: Int,
-    val RecipeID: Int,
-    val IngredientID: Int
-)
-
 @Entity(tableName = "Tag_List")
 data class Tag_List(
-    @PrimaryKey(autoGenerate = true) val TagID: Int,
-    val Tag: String
+    @PrimaryKey val Tag: String
 )
 
 @Entity(tableName = "Recipe_Tags")
 data class Recipe_tags(
     @PrimaryKey(autoGenerate = true) val PairKey: Int,
-    val RecipeID: Int,
-    val TagID: Int
-)
-
-@Entity(tableName = "Image_List")
-data class Image_List(
-    @PrimaryKey(autoGenerate = true) val ImageID: Int,
-    val FileName: String,
-    val RecipeID: Int
+    val RecipeName: String,
+    val Tag: String
 )
 
 @Entity(tableName = "Users")
@@ -66,14 +54,14 @@ interface Insertion {
     @Insert
     fun insertRecipeInfo(recipeInfo: Recipe_Info): Long
 
+    @Delete
+    fun deleteRecipe(recipeInfo: Recipe_Info)
+
     @Insert
     fun insertIngredientList(ingredientList: Ingredient_List): Long
 
     @Insert
     fun insertIngredientUse(ingredientUse: Ingredient_Use): Long
-
-    @Insert
-    fun insertRecipeIngredients(recipeIngredients: Recipe_Ingredients): Long
 
     @Insert
     fun insertTagList(tagList: Tag_List): Long
@@ -82,8 +70,6 @@ interface Insertion {
     fun insertRecipeTags(recipeTags: Recipe_tags): Long
 
     @Insert
-    fun insertImageList(imageList: Image_List): Long
-
     fun insertUser(user: Users): Long
 }
 
@@ -92,10 +78,8 @@ interface Insertion {
     Recipe_Info::class,
     Ingredient_List::class,
     Ingredient_Use::class,
-    Recipe_Ingredients::class,
     Tag_List::class,
     Recipe_tags::class,
-    Image_List::class
 ], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun insertion(): Insertion
@@ -107,35 +91,29 @@ interface ReadData{
     @Query("SELECT il.IngredientName, iu.Quantity, iu.Unit " +
             "FROM Recipe_Info ri, Ingredient_List il, Ingredient_Use iu " +
             "WHERE ri.recipeName = :searchName " +
-            "AND il.IngredientID = iu.IngredientID " +
-            "AND iu.RecipeID = ri.RecipeID")
+            "AND il.IngredientName = iu.IngredientName " +
+            "AND iu.RecipeName = ri.RecipeName")
     fun getRecipeIngredients(searchName: String): List<RecipeIngredientList>
 
     @Query("SELECT RecipeName " +
             "FROM Recipe_Info")
     fun getRecipeNames(): List<String>
 
-    @Query("SELECT il.FileName " +
-            "FROM Recipe_Info ri, Image_List il " +
-            "WHERE ri.RecipeName = :searchName " +
-            "AND il.RecipeID = ri.RecipeID")
-    fun getImageFileNamesByRecipeName(searchName: String): List<String>
 
     @Query("SELECT tl.tag " +
             "FROM Recipe_Info ri, Tag_List tl, Recipe_Tags rt " +
             "WHERE ri.recipeName = :searchName " +
-            "AND rt.RecipeID = ri.RecipeID " +
-            "AND tl.tagID = rt.tagID")
+            "AND rt.RecipeName = ri.RecipeName ")
     fun getTagsByRecipeName(searchName: String): List<String>
 
     @Query("SELECT RecipeDesc " +
             "FROM Recipe_Info " +
-            "WHERE RecipeID = :searchName")
+            "WHERE RecipeName = :searchName")
     fun getDesc(searchName: String): String
 
     @Query("SELECT RecipeInstructions " +
             "FROM Recipe_Info " +
-            "WHERE RecipeID = :searchName")
+            "WHERE RecipeName = :searchName")
     fun getInstructions(searchName: String): String
 
     @Query("SELECT Password " +
