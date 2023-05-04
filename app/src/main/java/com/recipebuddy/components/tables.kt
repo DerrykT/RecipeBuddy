019cdcb0c7a3ef22a33de271292d87693efbc80b
ngaiohawkins <114262503+ngaiohawkins@users.noreply.github.com>
2023-05-03 20:01:24 -0400

    Update tables.kt

--- app/src/main/java/com/recipebuddy/components/tables.kt
+++ app/src/main/java/com/recipebuddy/components/tables.kt
@@ -1,15 +1,16 @@
-package com.recipebuddy.components
+package com.recipebuddy.database
 
 import androidx.room.*
+import com.recipebuddy.util.Recipe
 
 @Entity(tableName = "Recipe_Info")
 data class Recipe_Info(
     @PrimaryKey val RecipeName: String,
-    val RecipeDesc: String,
+    val RecipeTimers: String,
     val RecipeInstructions: String,
-    val RecipeRating: Double,
+    val RecipeRating: Int,
     val AssociatedUser: String,
-    val Time: String,
+    val Time: Int,
     val Picture: Int,
     val Tools: String
 )
@@ -21,9 +22,13 @@ data class Ingredient_List(
     val Unit: String
 )
 
-@Entity(tableName = "Ingredient_Use")
+@Entity(tableName = "Tools_List")
+data class Tool_List(
+    @PrimaryKey val ToolName: String
+)
+
+@Entity(tableName = "Ingredient_Use", primaryKeys = ["IngredientName", "RecipeName"])
 data class Ingredient_Use(
-    @PrimaryKey(autoGenerate = true) val UsageKey: Int,
     val IngredientName: String,
     val RecipeName: String,
     val Quantity: Int,
@@ -35,9 +40,8 @@ data class Tag_List(
     @PrimaryKey val Tag: String
 )
 
-@Entity(tableName = "Recipe_Tags")
-data class Recipe_tags(
-    @PrimaryKey(autoGenerate = true) val PairKey: Int,
+@Entity(tableName = "Recipe_Tags", primaryKeys = ["RecipeName", "Tag"])
+data class Recipe_Tags(
     val RecipeName: String,
     val Tag: String
 )
@@ -67,20 +71,25 @@ interface Insertion {
     fun insertTagList(tagList: Tag_List): Long
 
     @Insert
-    fun insertRecipeTags(recipeTags: Recipe_tags): Long
+    fun insertRecipeTags(recipeTags: Recipe_Tags): Long
 
     @Insert
     fun insertUser(user: Users): Long
+
+    @Insert
+    fun insertTool(tool: Tool_List): Long
 }
 
 
 @Database(entities = [
     Recipe_Info::class,
     Ingredient_List::class,
+    Tool_List::class,
     Ingredient_Use::class,
     Tag_List::class,
-    Recipe_tags::class,
-], version = 1)
+    Recipe_Tags::class,
+    Users::class,
+], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun insertion(): Insertion
     abstract fun readData(): ReadData
@@ -99,6 +108,17 @@ interface ReadData{
             "FROM Recipe_Info")
     fun getRecipeNames(): List<String>
 
+    @Query("SELECT * FROM Tag_List")
+    fun getTags(): List<Tag_List>
+
+    @Query("SELECT * FROM Recipe_Info")
+    fun getRecipes(): List<Recipe_Info>
+
+    @Query("SELECT * FROM Ingredient_List")
+    fun getIngredients(): List<Ingredient_List>
+
+    @Query("SELECT ToolName FROM Tools_List")
+    fun getTools(): List<String>
 
     @Query("SELECT tl.tag " +
             "FROM Recipe_Info ri, Tag_List tl, Recipe_Tags rt " +
@@ -106,15 +126,23 @@ interface ReadData{
             "AND rt.RecipeName = ri.RecipeName ")
     fun getTagsByRecipeName(searchName: String): List<String>
 
-    @Query("SELECT RecipeDesc " +
-            "FROM Recipe_Info " +
-            "WHERE RecipeName = :searchName")
-    fun getDesc(searchName: String): String
+    //get list of recipe names that contain a specific tag
+    @Query ("SELECT rt.RecipeName " +
+             "FROM Recipe_Tags rt " +
+             "WHERE rt.Tag = :selectedTag ")
+    fun getRecipeNamesByTag(selectedTag: String): List<String>
 
-    @Query("SELECT RecipeInstructions " +
-            "FROM Recipe_Info " +
-            "WHERE RecipeName = :searchName")
-    fun getInstructions(searchName: String): String
+    //get list of recipes from recipe_info where recipe name = searchName
+    @Query("SELECT * " +
+            "FROM Recipe_Info ri " +
+            "WHERE ri.RecipeName = :searchName ")
+    fun getRecipeListByRecipeName(searchName: String?): List<Recipe>
+
+    //get list of recipes from recipe_info where rating = passedRating
+    @Query("SELECT * " +
+            "FROM Recipe_Info ri " +
+            "WHERE ri.RecipeRating >= :passedRating ")
+    fun getRecipeListByRating(passedRating: Int?): List<Recipe>
 
     @Query("SELECT Password " +
             "FROM Users " +
